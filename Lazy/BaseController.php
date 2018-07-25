794f25934d313341a27318a8689cf589e2871fa9
tjtgzxs <tjtgzxs@gmail.com>
2018-07-25 15:05:06 +0800

    modifie Smarty

--- Lazy/BaseController.php
+++ Lazy/BaseController.php
@@ -12,16 +12,17 @@ namespace Lazy;
 class BaseController
 {
     public $view_file='';
-    protected $smarty;
     public function __construct()
     {
-        $this->smarty=new \Smarty();
-        $this->smarty->setTemplateDir(APP.'View'.DIRECTORY_SEPARATOR);
+       BaseView::init();
     }
 
-    protected  function render($data=[],$view_file=''){
+    protected function assign($key,$value=null){
+        BaseView::assign($key,$value);
+    }
+    protected  function render($view_file=''){
 
-                BaseView::display($data,$view_file);
+                BaseView::display($view_file);
     }
 
 }
--- Lazy/BaseView.php
+++ Lazy/BaseView.php
@@ -12,19 +12,38 @@ namespace Lazy;
 class BaseView
 {
     private  static $template_files;
-    public static function display($data,$view_file=''){
-        if(is_array($data)){
-            extract($data);
-        }
+    protected  static $smarty;
+    public  static function init()
+    {
+        require SMARTY.'Smarty.class.php';
+        self::$smarty=new \Smarty();
+        self::$smarty->setTemplateDir(APP.'View'.DIRECTORY_SEPARATOR);
+        self::$smarty->setCompileDir(CACHE.'smarty_compiled');//定义smarty编译文件的缓存路径
+        self::$smarty->debugging=true;//开启错误控制台
+        self::$smarty->caching=true;//开启缓存
+        self::$smarty->cache_lifetime=true;//开启缓存生存时间
+    }
+
+    public static function assign($key,$value=null){
+
+        self::$smarty->assign($key,$value);
+    }
+
+    public static function display($view_file=''){
+//        if(is_array($data)){
+//            extract($data);
+//        }
         if(empty($view_file)){
             $view_file=strtolower(ACTION);
         }
-        //start to render
-        ob_start();
-        ob_implicit_flush(0);
-        include APP."View".DIRECTORY_SEPARATOR.CONTROLLER.DIRECTORY_SEPARATOR.$view_file.VEXT;
-        $content=ob_get_clean();
-        echo $content;
+
+        self::$smarty->display(APP."View".DIRECTORY_SEPARATOR.CONTROLLER.DIRECTORY_SEPARATOR.$view_file.VEXT);
+//        //start to render
+//        ob_start();
+//        ob_implicit_flush(0);
+//        include APP."View".DIRECTORY_SEPARATOR.CONTROLLER.DIRECTORY_SEPARATOR.$view_file.VEXT;
+//        $content=ob_get_clean();
+//        echo $content;
     }
 
 }
