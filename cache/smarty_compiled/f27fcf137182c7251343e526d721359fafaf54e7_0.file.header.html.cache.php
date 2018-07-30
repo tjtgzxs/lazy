8394f8ff1c61e82f1d347ae0721ca7a3d95631bd
Johnson <tjtgzxs@outlook.com>
2018-07-30 14:37:53 +0000

    add static html

---
+++ cache/smarty_compiled/f27fcf137182c7251343e526d721359fafaf54e7_0.file.header.html.cache.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 3.1.32, created on 2018-07-30 14:32:49
+  from '/home/vagrant/Code/lazy/app/View/static/header.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32',
+  'unifunc' => 'content_5b5f2191593a12_06322324',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f27fcf137182c7251343e526d721359fafaf54e7' => 
+    array (
+      0 => '/home/vagrant/Code/lazy/app/View/static/header.html',
+      1 => 1532961169,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5b5f2191593a12_06322324 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '15300875635b5f219152fa37_52189523';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php if ($_smarty_tpl->tpl_vars['title']->value) {
+echo $_smarty_tpl->tpl_vars['title']->value;
+} else { ?>LazyBlog<?php }?></title>
+</head>
+<body>
+
+</body>
+</html><?php }
+}
