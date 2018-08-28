041a56ab2b01a1c635e13f7c45f167121871202d
tjtgzxs <tjtgzxs@gmail.com>
2018-08-28 16:39:55 +0800

    reformat

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -5,7 +5,9 @@
  * Date: 2018/7/22
  * Time: 19:43
  */
+
 namespace Controller;
+
 use Lazy\BaseController;
 use Lazy\CommonFunction;
 use Model\IndexModel;
@@ -21,38 +23,40 @@ class IndexController extends BaseController
     /**
      * get Index Page
      */
-    public function IndexAction(){
-         $m=new IndexModel();
-         $banners=$m->getAll('lazy_banner','*',['is_del'=>0,'is_show'=>1],['order_by'=>'ASC']);
-         $cates=$m->getAll('lazy_cate','*',['is_del'=>0,'parent_id'=>0,'is_show'=>1]);
-         $arr=[];
-         $cate_arr=[];
-         foreach ($cates as $k=>$v){
-             $arr['name']=$v['name'];
-             $arr['id']=$v['id'];
-             $arr['img_url']=empty($v['img_url'])?DEFAULT_FANG:$v['img_url'];
-             $sub_cate=$m->getAll('lazy_cate','*',['is_del'=>0,'parent_id'=>$v['id']]);
-             $cat_id=array_column($sub_cate,'id');
-             array_push($cat_id,$v['id']);
-             $arr['article']=$m->getAll('lazy_article','*',['is_del'=>0,'cat_id'=>$cat_id],['update_date'=>'DESC'],0,5);
-             array_push($cate_arr,$arr);
-         }
-         $this->assign('arr',$cate_arr);
-         $this->assign('title','Welcome to Lazy Blog');
-         $this->assign('banners',$banners);
-         $this->render();
+    public function IndexAction()
+    {
+        $m = new IndexModel();
+        $banners = $m->getAll('lazy_banner', '*', ['is_del' => 0, 'is_show' => 1], ['order_by' => 'ASC']);
+        $cates = $m->getAll('lazy_cate', '*', ['is_del' => 0, 'parent_id' => 0, 'is_show' => 1]);
+        $arr = [];
+        $cate_arr = [];
+        foreach ($cates as $k => $v) {
+            $arr['name'] = $v['name'];
+            $arr['id'] = $v['id'];
+            $arr['img_url'] = empty($v['img_url']) ? DEFAULT_FANG : $v['img_url'];
+            $sub_cate = $m->getAll('lazy_cate', '*', ['is_del' => 0, 'parent_id' => $v['id']]);
+            $cat_id = array_column($sub_cate, 'id');
+            array_push($cat_id, $v['id']);
+            $arr['article'] = $m->getAll('lazy_article', '*', ['is_del' => 0, 'cat_id' => $cat_id], ['update_date' => 'DESC'], 0, 5);
+            array_push($cate_arr, $arr);
+        }
+        $this->assign('arr', $cate_arr);
+        $this->assign('title', 'Welcome to Lazy Blog');
+        $this->assign('banners', $banners);
+        $this->render();
 //       $this->render(['k'=>1]);
     }
 
-    public function searchAction(){
-        $page_limit=!empty($limit)? $limit:LIMIT;
-        $start=((isset($_GET['page'])?$_GET['page']:1)-1)*$page_limit;
-        $keyWorld=$_REQUEST['keywords'];
-        $list=CommonFunction::getSearch($keyWorld);
-        $this->assign('title','Search Result');
-        $this->assign('list',$list);
-        $this->assign('max_page',ceil(count($list)/$page_limit));
-        $this->assign('page',!empty($_GET['page'])?$_GET['page']:1);
+    public function searchAction()
+    {
+        $page_limit = !empty($limit) ? $limit : LIMIT;
+        $start = ((isset($_GET['page']) ? $_GET['page'] : 1) - 1) * $page_limit;
+        $keyWorld = $_REQUEST['keywords'];
+        $list = CommonFunction::getSearch($keyWorld);
+        $this->assign('title', 'Search Result');
+        $this->assign('list', $list);
+        $this->assign('max_page', ceil(count($list) / $page_limit));
+        $this->assign('page', !empty($_GET['page']) ? $_GET['page'] : 1);
         $this->render('search_list');
     }
 
